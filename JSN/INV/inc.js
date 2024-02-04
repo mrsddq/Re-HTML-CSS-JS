e1e2c9cda64a1cb8ea02be7d4caca050d6b11708
Laraib <laraibks@gmail.com>
2024-02-05 00:44:24 +0530

    DataTypes

---
+++ JSN/INV/inc.js
@@ -0,0 +1,24 @@
+// let y=null;
+// let z;
+// console.log(typeof z);
+// z=y;
+// console.log(typeof z)
+
+// const num=Infinity/Infinity
+// console.log(num)
+
+// let myVar;
+// console.log(myVar===null);
+
+// let text1="20";
+// let text2="5";
+// let result=text1<text2;
+// console.log(result);
+
+// let a=5;
+// console.log(a);
+// console.log(a++);
+// console.log(a++);
+
+// console.log(0||"hello"||true);
+
