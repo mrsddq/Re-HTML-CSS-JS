ed041f808040b4fbf41cbd49f6f64702fb9aac2d
Laraib <laraibks@gmail.com>
2024-03-29 15:13:02 +0530

    loops on arrays

--- JS/LoopsArraysFunctionAnd Objects/LoopOverArrays/script.js
+++ JS/LoopsArraysFunctionAnd Objects/LoopOverArrays/script.js
@@ -1,11 +1,11 @@
-function sum(num1, num2){
-    console.log("Sum of the numbers is:"+ (num1+num2)
-    );
-    console.log(`Sum of the numbers is: ${num1+num2}`
-    );
-    console.log("Sum of the numbers is:"+ num1 + num2
-    );
-}
+const students=[
+    'Tina',
+    'Laraib',
+    'Seema',
+    'Alex',
+    'Sita'
+];
 
-sum(4,5);
-sum(4);
+for (let i=0;i=students.length;i++){
+    console.log(`Roll no ${i+1}: ${students[i]}`);
+}
